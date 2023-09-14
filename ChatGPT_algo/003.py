6db90998506d7f5ee0a00564dc234c2c62dd397d
ZhouLu <lz354000575@gmail.com>
2023-09-14 14:38:01 +0900

    update 003

--- ChatGPT_algo/003.py
+++ ChatGPT_algo/003.py
@@ -58,8 +58,75 @@ def merge1_1(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
 空间复杂度 O(1)
 '''
 
-#1_2 非重新分配空间
-#TODO
+#2 三指针 非重新分配空间
+def merge2(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+
+    #先扩张nums1，确保列表长度
+    nums1.extend([None]*(m+n-len(nums1)))
+    print("update ",nums1 )
+
+    # 定义三个指针, 
+    # i为nums1末尾指针，
+    # j为nums2末尾指针，
+    # point为nums1中的插入指针
+    i, j, point =m-1,n-1,m+n-1
+
+    #print(len(nums1),point)
+
+    while j >= 0 and i >=0:
+        if nums2[j] >= nums1[i] :
+            nums1[point] = nums2[j]
+            j -= 1
+        else:
+            nums1[point] = nums1[i]
+            i -= 1
+        point -= 1
+
+    #[1] 如果 i 变成了负数 nums1[i]不存在
+    if i < 0:
+        for j in range(j,-1,-1):
+            nums1[point] = nums2[j]
+            point -=1
+    
+    print(nums1)
+
+#2_1 [1]优化
+def merge2_1(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+
+    nums1.extend([None]*(m+n-len(nums1)))
+    i, j, point =m-1,n-1,m+n-1
+
+    #原始判断条件
+    '''
+    while j >= 0:
+        if i >= 0:
+            if nums2[j] < nums1[i]:
+                nums1[point] = nums1[i]
+                i -= 1
+            else:
+                nums1[point] = nums2[j]
+                j -= 1
+        else:
+            nums1[point] = nums2[j]
+            j -= 1
+
+        point -= 1
+    '''
+
+    #合并后判断条件
+    while j >= 0:
+        #条件多的适合放进if
+        if i >= 0 and nums2[j] < nums1[i]:
+            nums1[point] = nums1[i]
+            i -= 1  
+        else:
+            #考虑到运行效率，相等的情况下从nums2取出较好
+            nums1[point] = nums2[j]
+            j -= 1
+
+        point -= 1
+
+    print(nums1)
 
 # main()
 # input1 = input("数组1：")
@@ -67,11 +134,11 @@ def merge1_1(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
 # input2 = input("数组2：")
 # input2_1 = input("有效数字：")
 
-# test1
-input1 = "1,2,4,7"
-input1_1 = "4"
-input2 = "3,6,8"
-input2_1 = "3"
+# # test1
+# input1 = "1,2,4,7"
+# input1_1 = "4"
+# input2 = "3,6,8"
+# input2_1 = "3"
 
 # #test2
 # input1 = "3,46,79,81"
@@ -79,9 +146,16 @@ input2_1 = "3"
 # input2 = "46,57,91"
 # input2_1 = "3"
 
+#test3
+input2 = "1,2,3"
+input1_1 = "3"
+input1 = "4,5,6"
+input2_1 = "3"
+
+
 nums1 = list(map(int,input1.split(",")))
 m = int(input1_1)
 nums2 = list(map(int,input2.split(",")))
 n = int(input2_1)
 
-merge1_1(nums1,m,nums2,n)
+merge2_1(nums1,m,nums2,n)
--- ChatGPT_algo/test.py
+++ ChatGPT_algo/test.py
@@ -1,13 +1,2 @@
-# 原始列表
-nums = [4, 2, 1, 9, 5]
-
-# 配对元素与其原始索引
-indexed_nums = [(num, index) for index, num in enumerate(nums)]
-print(indexed_nums)
-# 根据元素值进行排序
-indexed_nums.sort()
-print(indexed_nums)
-# 提取排序后的原始索引
-sorted_indices = [index for num, index in indexed_nums]
-
-print("排序后的原始索引:", sorted_indices)
+for i in range (5,0):
+    print(i)
