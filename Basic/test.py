ee472d16e1c0fb4d5f1596eff0f5ef9eb34ee7c8
ZhouLu <lz354000575@gmail.com>
2023-09-25 11:57:46 +0900

    update 05&05A

--- Basic/test.py
+++ Basic/test.py
@@ -1,2 +1,20 @@
-m = (0 + 0)//2
-print(m)
+class ListNode:
+    def __init__(self, key = -1, value = -1, next = None):
+        self.key = key
+        self.value = value
+        self.next = next
+
+map = [ListNode() for i in range(3)]
+print(map)
+node1 = ListNode()
+node2 = ListNode()
+
+map[0].next = None
+
+test_node = map[0].next
+
+if test_node:
+    print(test_node)
+    print(11111)
+else:
+    print("None")
