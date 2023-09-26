61467104a86ca6b6b032be3b2bd8dcf3edcbd14a
ZhouLu <lz354000575@gmail.com>
2023-09-26 17:40:29 +0900

    update 11_basic

--- Basic/test.py
+++ Basic/test.py
@@ -1,20 +1,6 @@
-class ListNode:
-    def __init__(self, key = -1, value = -1, next = None):
-        self.key = key
-        self.value = value
-        self.next = next
+my_list = []
 
-map = [ListNode() for i in range(3)]
-print(map)
-node1 = ListNode()
-node2 = ListNode()
-
-map[0].next = None
-
-test_node = map[0].next
-
-if test_node:
-    print(test_node)
-    print(11111)
+if not my_list:
+    print("The list is empty.")
 else:
-    print("None")
+    print("The list is not empty.")
