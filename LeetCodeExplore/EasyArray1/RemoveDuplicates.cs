9c7bc651cfc90c048af01e130d0a5eb23d2c5529
RikusyuN <zhoul@nangokrstudios.jp>
2025-02-17 10:50:55 +0900

    EasyArray1

---
+++ LeetCodeExplore/EasyArray1/RemoveDuplicates.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using System.Linq;
+
+/// <summary>
+/// hashset 执行检索和存储
+/// </summary>
+public class Solution
+{
+    static void Main()
+    {
+        var nums = new int[]{1,1,2};
+        RemoveDuplicates(nums);
+    }
+
+    static int RemoveDuplicates(int[] nums)
+    {
+        int index = 0;
+        int index2 = 0;
+        int[] newArray = new int[nums.Length];
+        // foreach (var num in nums)
+        // {
+        //     Console.Write(num+" ");
+        //     if (!newArray.Contains(num))
+        //     {
+        //         newArray[index2] = num;
+        //         index2 ++;
+        //     }
+        //     index++;
+        // }
+
+
+        foreach(var n in newArray)
+        {
+            Console.Write(n);
+        }
+        return newArray.Length;
+    }
+}
--- LeetCodeExplore/README.md
+++ LeetCodeExplore/README.md
@@ -1,8 +1,14 @@
 LeetCode Explore
-
-
 Top Interview Questions  
-
 [Easy Collection](https://leetcode.com/explore/interview/card/top-interview-questions-easy/)
+11 8 6 5 2 4 2 4 6
+
 
 [Medium Collection](https://leetcode.com/explore/interview/card/top-interview-questions-medium/)
+8 3 7 5 9 4 4 7 5
+
+指令创建项目文件
+dotnet new console -o MyAlgorithmProject
+code .
+
+dotnet new console -o EasyArray1
